f02bfe236c191c47df424777796749b053cb7cd6
arinazaikina <sav2405@gmail.com>
2023-07-18 22:55:42 +0300

    hw 26.1

--- app_user/apps.py
+++ app_user/apps.py
@@ -1,7 +1,16 @@
 from django.apps import AppConfig
 
+from .tasks import create_periodic_task
+
 
 class AppUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_user'
     verbose_name = 'Пользователи'
+
+    def ready(self):
+        """
+        При запуске приложения создаёт периодическую задачу
+        на проверку статуса платежей, если она еще не создана.
+        """
+        create_periodic_task()
